2efdda4761349511e29e04ad2e824fa4aca5c621
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2012-08-24 08:31:10 +0200

    Update of baseline after third test-drive.

--- src/test/webapp/lib/testutils.js
+++ src/test/webapp/lib/testutils.js
@@ -1,23 +1,11 @@
 jasmineui.inject(function () {
 
-    // disable transitions and speed up timeout during ui tests for better test performance
-    function jqueryMobileSpeedup() {
-        // Allow at most 20ms as timeouts.
-        var oldTimeout = window.setTimeout;
-        window.setTimeout = function (fn, delay) {
-            if (delay > 20) {
-                delay = 20;
-            }
-            return oldTimeout.call(this, fn, delay);
-        };
-
-        // Disable transitions
-        beforeLoad(function () {
-            $.mobile.defaultPageTransition = "none";
-            $.mobile.defaultDialogTransition = "none";
-        });
-    }
-
-    jqueryMobileSpeedup();
+    var oldTimeout = window.setTimeout;
+    window.setTimeout = function (fn, delay) {
+        if (delay > 20) {
+            delay = 20;
+        }
+        return oldTimeout.call(this, fn, delay);
+    };
 
 });
