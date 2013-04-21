f9bcbb6f4560351458f4520c3d3f4aa6d46f4f27
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-04-21 15:46:36 +0200

    Sync with master after first walkthrough.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,89 +1,88 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      files: ['app/**/*', 'test/**/*'],
-      tasks: ['jshint', 'karma:dev:run']
-    },
-    jshint: {
-      files: ['app/js/**/*.js', 'test/**/*Spec.js'],
-      options: {
-        strict: false,
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: false,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        trailing: true,
-        globals: {
-          module: false,
-          describe: true,
-          ddescribe: true,
-          xdescribe: true,
-          beforeEach: true,
-          afterEach: true,
-          it: true,
-          xit: true,
-          iit: true,
-          runs: true,
-          waitsFor: true,
-          waits: true,
-          spyOn: true,
-          expect: true,
-          jasmine: true,
-          uitest: true,
-          window: true,
-          document: true,
-          MockHttpServer: true,
-          dump: true
-        }
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 9000,
-          base: './',
-          hostname: ''
-        }
-      }
-    },
-    karma: {
-      options: {
-        configFile: 'test/config/karma.conf.js',
-        autoWatch: false
-      },
-      dev: {
-        options: {
-          singleRun: false,
-          browsers: ['Chrome']
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            files: ['app/**/*', 'test/**/*'],
+            tasks: ['jshint', 'karma:dev:run']
         },
-        background: true
-      },
-      ci: {
-        options: {
-          singleRun: true,
-          browsers: ['PhantomJS']
+        jshint: {
+            files: ['app/js/**/*.js', 'test/**/*Spec.js'],
+            options: {
+                strict: false,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: false,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                trailing: true,
+                globals: {
+                    module: false,
+                    describe: true,
+                    ddescribe: true,
+                    xdescribe: true,
+                    beforeEach: true,
+                    afterEach: true,
+                    it: true,
+                    xit: true,
+                    iit: true,
+                    runs: true,
+                    waitsFor: true,
+                    waits: true,
+                    spyOn: true,
+                    expect: true,
+                    jasmine: true,
+                    uitest: true,
+                    uit: true,
+                    window: true,
+                    document: true,
+                    MockHttpServer: true,
+                    dump: true
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    base: './',
+                    hostname: ''
+                }
+            }
+        },
+        karma: {
+            options: {
+                configFile: 'test/config/karma.conf.js',
+                autoWatch: false
+            },
+            dev: {
+                options: {
+                    singleRun: false,
+                    browsers: ['Chrome']
+                },
+                background: true
+            },
+            ci: {
+                options: {
+                    singleRun: true,
+                    browsers: ['PhantomJS']
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('dev', ['connect','karma:dev','watch']);
+    grunt.registerTask('default', ['jshint', 'karma:ci']);
+    grunt.registerTask('dev', ['connect', 'karma:dev', 'watch']);
+    grunt.registerTask('travis', ['default']);
 
-  grunt.registerTask('default', ['jshint','karma:ci']);
-
-  grunt.registerTask('travis', ['default']);
-
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 };
