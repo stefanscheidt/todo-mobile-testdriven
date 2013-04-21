ea8291045696f6fcd96489f7925899f548e61fc7
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-04-21 15:13:52 +0200

    Walkthrough, End of Part II.

--- app/js/todo.js
+++ app/js/todo.js
@@ -5,6 +5,7 @@
     $(function() {
         input = $('#todoPage_input');
         list = $('#todoPage_list');
+
         input.on("keypress", function (event) {
             if (event.keyCode !== 13) {
                 return;
@@ -12,16 +13,34 @@
             addEntry();
             input.val('');
         });
+
+        $.ajax('/todo-mobile/store').then(addEntries);
     });
 
     function addEntry() {
         var index = list.find('.entry').length;
-        list.append(entryHtml(input.val(), index));
-        list.parent().trigger('create');
+        appendToList(input.val(), index);
+        updateUi();
+    }
+
+    function addEntries(entries) {
+        for (var i = 0; i < entries.length; i++) {
+            var index = list.find('.entry').length;
+            appendToList(entries[i].text, index);
+        }
+        updateUi();
+    }
+
+    function appendToList(value, index) {
+        list.append(entryHtml(value, index));
     }
 
     function entryHtml(entry, index) {
         return '<input type="checkbox" id="todo' + index + '"><label for="todo' + index + '" class="entry">' + entry + '</label>';
+    }
+
+    function updateUi() {
+        list.parent().trigger('create');
     }
 
 })(window.jQuery);
--- test/ui/todoUiSpec.js
+++ test/ui/todoUiSpec.js
@@ -1,5 +1,53 @@
-describe('todo', function() {
+describe('todo', function () {
     uit.url('../../app/index.html');
+
+    var deferred;
+    var someEntryText = "Entry";
+
+    uit.append(function ($) {
+        deferred = $.Deferred();
+        spyOn($, 'ajax').andReturn(deferred);
+    });
+
+    it('shows todo page when visiting the app', function () {
+        uit.runs(function () {
+            expect('todoPage').toBeActivePage();
+        });
+    });
+
+    it('adds new entry to list', function () {
+        uit.runs(function () {
+            pressEnterOn(input().val(someEntryText + " 1"));
+            expect(entries().length).toBe(1);
+            expect(textOf(entries().eq(0))).toBe(someEntryText + " 1");
+            pressEnterOn(input().val(someEntryText + " 2"));
+            expect(entries().length).toBe(2);
+            expect(textOf(entries().eq(1))).toBe(someEntryText + " 2");
+        });
+    });
+
+    it('clears input after enter pressed', function () {
+        uit.runs(function () {
+            pressEnterOn(input().val(someEntryText));
+            expect(input().val()).toBe('');
+        });
+    });
+
+    it('loads entries from backend on load', function () {
+        uit.runs(function ($) {
+            expect($.ajax).toHaveBeenCalledWith('/todo-mobile/store');
+        });
+    });
+
+    it('updates list with loaded data', function () {
+        uit.runs(function () {
+            deferred.resolve([
+                {text: someEntryText}
+            ]);
+            expect(entries().length).toBe(1);
+            expect(textOf(entries())).toBe(someEntryText);
+        });
+    });
 
     function input() {
         return uit.inject(function ($) {
@@ -24,31 +72,5 @@ describe('todo', function() {
             return $.trim(element.text());
         });
     }
-
-    var someEntryText = "Entry";
-
-    it('shows todo page when visiting the app', function() {
-        uit.runs(function () {
-            expect('todoPage').toBeActivePage();
-        });
-    });
-
-    it('adds new entry to list', function () {
-        uit.runs(function () {
-            pressEnterOn(input().val(someEntryText + " 1"));
-            expect(entries().length).toBe(1);
-            expect(textOf(entries().eq(0))).toBe(someEntryText + " 1");
-            pressEnterOn(input().val(someEntryText + " 2"));
-            expect(entries().length).toBe(2);
-            expect(textOf(entries().eq(1))).toBe(someEntryText + " 2");
-        });
-    });
-
-    it('clears input after enter pressed', function () {
-        uit.runs(function () {
-            pressEnterOn(input().val(someEntryText));
-            expect(input().val()).toBe('');
-        });
-    });
 
 });
